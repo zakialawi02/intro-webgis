0e605dc4fe044550810367ba5bfc4e04efecd4be
zakialawi02 <zaki.alawi22@gmail.com>
2023-11-26 18:33:03 +0700

    mini project lanjutan

--- 11-mini-project-lanjutan/assets/js/script.js
+++ 11-mini-project-lanjutan/assets/js/script.js
@@ -8,3 +8,16 @@ menu.addEventListener("click", (e) => {
     menu.classList.remove("show");
     console.log("close");
 });
+
+$("#layer-toggle").click(function (e) {
+    $(".layer-panel").toggleClass('layer-panel-show');
+});
+
+// Menampilkan dan menyembunyikan modal
+$("#info-toggle").click(function () {
+    $("#modal1").toggleClass("show-modal");
+});
+
+$("#closeModalBtn, .modal").click(function () {
+    $("#modal1").removeClass("show-modal");
+});
